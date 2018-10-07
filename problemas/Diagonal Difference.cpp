0be41af3c48f3e8c024a2cf726cb1d04c555020a
SaSu <floresjeremiasxd123@gmail.com>
2018-10-07 13:17:14 -0500

    lvl1

---
+++ problemas/Diagonal Difference.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){	
+	int n,a,b=0,c=0;
+	for(int c=0;c<n;c++){
+		for(int j=0;j<n;j++){
+			cin>>a;
+			if(c==j) b+=a;
+			if(c+j=n-1) c+=a;  
+		}
+	}
+	cout<<((a>b)?a-b:b-a)<<"\n";
+	return 0;
+}
